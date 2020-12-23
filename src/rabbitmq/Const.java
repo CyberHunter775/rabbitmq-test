a045762aee6ccdeaa83e4de011f4abd79404d699
zebragg <xin11058@163.com>
2020-12-23 16:07:35 +0800

    Update Const.java

--- src/rabbitmq/Const.java
+++ src/rabbitmq/Const.java
@@ -4,7 +4,7 @@ public class Const {
 
     // 正式环境
     public final static String QUEUE_NAME = "test.test.test";
-	public final static String filepath = "D://test/test2.txt";
+	public final static String filepath = "D://test//test2.txt";
 
     public final static String host = "127.0.0.1";
     public final static int port = 5672;
